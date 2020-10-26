e9e86bb7f355c626d59a70b029f34808f87759b0
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2020-10-26 20:37:31 +0530

    Fixed error

--- covid_detect/settings.py
+++ covid_detect/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4e@j1r4@7cr#iw1&a98!(^#za-#5t#-gy4owk5$!zgv)0_bzcj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'root.apps.RootConfig',
+    
     'crispy_forms',
 ]
 
